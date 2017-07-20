062e1e70efe659402eb32e02699c1367a57aedd5
SNEHA GHOSH <snehaghosh91@gmail.com>
2017-07-20 18:45:08 -0400

    Update init.go

--- core/init.go
+++ core/init.go
@@ -16,8 +16,9 @@ package main
 import (
 	"errors"
 	"fmt"
-
+	//"strconv"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	//pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 // SimpleChaincode example simple Chaincode implementation
@@ -54,6 +55,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
+	} else if function == "init_user" {
+		return t.init_user(stub, args)
+	} else if function == "init_project" {
+		return t.init_project(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)
 
